e13a72fdfa9215e50ed60850bbf5c4ad3dccae37
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-12 23:04:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,14 +166,17 @@ def team_names
 end
 
 def player_numbers (team_name)
+  nums = []
   game_hash.each do |_place, team|
-    next unless attribute == :number
+    next unless team[:team_name] == team_name
     team.each do |attribute, data|
-      data.each do |player|
-        return player[:shoe] if player[:player_name] == sought_player_name
+      next unless attribute == :players
+      data.each do |data|
+        nums << data[:number]
       end
     end
   end
+  nums
 end
 
 
