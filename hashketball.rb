28399fa232f755c069fc41f75e76b5b685a296e7
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-12 23:59:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -184,12 +184,13 @@ def player_stats(sought_player_name)
   game_hash.collect do |place, team|
     team.each do |attribute, _data|
       next unless attribute == :players
+      
+game_hash[place][attribute].each do |player|
+  next unless player[:player_name] == sought_player_name
+  binding.pry
 
-      game_hash[place][attribute].each do |player|
-        next unless player[:player_name] == sought_player_name
-
-        new_hash = player.delete_if do |k, _v|
-          k == :player_name
+new_hash = player.delete_if do |k, _v|
+    k == :player_name
         end
       end
     end
